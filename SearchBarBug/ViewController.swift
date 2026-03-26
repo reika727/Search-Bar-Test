1bf35182bd5b215936e3cde455e343ec5056d627
reika727 <rei.kasuya@l-is-b.com>
2026-03-26 16:17:54 +0900

    Add table

--- SearchBarBug/ViewController.swift
+++ SearchBarBug/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UISplitViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        self.preferredDisplayMode = .oneBesideSecondary
+
+        self.viewControllers = [
+            UINavigationController(rootViewController: TableViewController())
+        ]
     }
-
-
 }
 
+class TableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.searchController = UISearchController(searchResultsController: nil)
+    }
+
+    override func tableView(_: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+
+    override func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        3
+    }
+}
