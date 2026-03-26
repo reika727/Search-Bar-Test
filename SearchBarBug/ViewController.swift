9ac7f458807bd6ea84f1c5c8fcf7b39b1924b8c5
reika727 <rei.kasuya@l-is-b.com>
2026-03-26 12:49:18 +0900

    Initial Commit

---
+++ SearchBarBug/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchBarBug
+//
+//  Created by rei.kasuya on 2026/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
